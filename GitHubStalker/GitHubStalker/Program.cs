8fbb9b5bed0af68dd8defed468295ceae67425cc
Rebecca Mooney <astrakiseki@gmail.com>
2016-04-08 11:51:03 -0700

    White space removal

--- GitHubStalker/GitHubStalker/Program.cs
+++ GitHubStalker/GitHubStalker/Program.cs
@@ -6,10 +6,6 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
-What I need to do is figure out where to separate classes, learn APIs, and how to seek out the information I need.  Info seeking class set is necessary, VERY necessary.
-ANNND DISREGARD THAT, JSON IS INDEED AMAZING.  And github's a jerk.
-*/
 namespace GitHubStalker
 {
     class Program
@@ -29,15 +25,11 @@ namespace GitHubStalker
 
             //parse  json into something I can display
             var o = JObject.Parse(json);
-
-
-
             Console.WriteLine(o["login"].ToString());
             Console.WriteLine(o["login"].ToString() + " has " + o["public_repos"].ToString() + " public repositories and " + o["followers"].ToString() + " followers.  They are following " + o["following"].ToString() + " accounts." );
 
             Console.ReadLine();
 
-        
             Console.WriteLine("Would you like to look at their repositories?  Y/N");
 
             string RepoAnswer = Console.ReadLine();
@@ -46,7 +38,6 @@ namespace GitHubStalker
                 {
                 string ReposList = wc.DownloadString("http://api.github.com/users/" + username + "/repos");
                 var r = JArray.Parse(ReposList);
-
                 Console.WriteLine("Url: https:/github.com/{0}", username);
                 Console.WriteLine("{0} has " + o["public_repos"].ToString() + " repositories, consisting of: ", username);
                 foreach (var Repo in r)
@@ -61,12 +52,10 @@ namespace GitHubStalker
                 Console.WriteLine("Thank you for using this program!");
                 Console.ReadLine();
             }
-
             else
             {
                 Console.WriteLine("Please answer Y or N.");
             }
-
             Console.ReadLine();
         }
     }
